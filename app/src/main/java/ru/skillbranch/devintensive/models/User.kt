6dc228352f8cbbf9e6f339d215c78350e2dd5088
dino944 <dino944@gmail.com>
2019-06-30 21:10:22 +0300

    hometask_2 complete

---
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -0,0 +1,77 @@
+package ru.skillbranch.devintensive.models
+
+import ru.skillbranch.devintensive.utils.Utils
+import java.util.*
+
+/**
+ * Created by Dino944 jn 28.06.2019.
+ **/
+data class User (
+        val id: String,
+        var firstName: String?,
+        var lastName: String?,
+        var avatar: String?,
+        var rating: Int = 0,
+        var respect: Int = 0,
+        val lastVisit: Date? = null,
+        val isOnline: Boolean = false
+//        var introBit :String// = "$firstName $lastName !!!!!!"
+)
+
+{
+//    var introBit :String
+//    var introBit :String =getIntro()
+
+    constructor(id: String, firstName: String?, lastName: String?) : this(
+            id = id,
+            firstName = firstName,
+            lastName = lastName,
+            avatar = null
+    )
+
+    constructor(id: String) : this(id, "John", "Doe")
+
+    init {
+//        introBit = getIntro()
+        println("It's Alive!!!\n" +
+                "${if(lastName==="Doe") "His name id $firstName $lastName" else "And his name is $firstName $lastName!!!!" }\n" )
+ //       "${getIntro()}")
+    }
+    companion object Factory{
+        private var lastId : Int = -1
+        fun makeUser(fullName:String?) : User {
+            lastId++
+
+            val (firstName, lastName) = Utils.parseFullName(fullName)
+
+
+
+            return User(id= "$lastId", firstName = firstName, lastName = lastName)
+        }
+    }
+ /**   private fun getIntro()="""
+        df df df dfdfdfdf !!!!
+        df df df dfffffff ...
+
+        df df df dfdfdfdf !!!!
+        df df df dfffffff ...
+        ${"\n"}
+
+        $firstName $lastName
+    """.trimIndent()
+
+    fun printMe() : Unit{
+        println("""
+        id: $id
+        firstName: $firstName
+        lastName: $lastName
+        avatar: $avatar
+        rating: $rating
+        respect: $respect
+        lastVisit: $lastVisit
+        isOnline: $isOnline
+        """.trimIndent())
+    }
+**/
+}
+
