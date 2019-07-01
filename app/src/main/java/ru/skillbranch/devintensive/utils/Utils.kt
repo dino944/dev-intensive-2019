1cd15231a323b8ce6436d209bab2127251a24527
dino944 <dino944@gmail.com>
2019-07-01 17:28:30 +0300

    hometask_2 complete

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -23,7 +23,55 @@ object Utils {
     }
 
     fun transliteration(payload: String, divider:String = " "): String {
-        return "PWW"
+//val TT:String = ""
+//       val Tt : String =
+        val Tt : String = payload.replace(Regex("[абвгдеёжзийклмнопрстуфхцшщъыьэюя ]")) {
+                    when(it.value) {
+                        "а"-> "a"
+                        "б"-> "b"
+                        "в"-> "v"
+                        "г"-> "g"
+                        " "-> " "
+                        "д"-> "d"
+                        "е"-> "e"
+                        "ё"-> "e"
+                        "ж"-> "zh"
+                        "з"-> "z"
+                        "и"-> "i"
+                        "й"-> "i"
+                        "к"-> "k"
+                        "л"-> "l"
+                        "м"-> "m"
+                        "н"-> "n"
+                        "о"-> "o"
+                        "п"-> "p"
+                        "р"-> "r"
+                        "с"-> "s"
+                        "т"-> "t"
+                        "у"-> "u"
+                        "ф"-> "f"
+                        "х"-> "h"
+                        "ц"-> "c"
+                        "ч"-> "ch"
+                        "ш"-> "sh"
+                        "щ"-> "sh'"
+                        "ъ"-> ""
+                        "ы"-> "i"
+                        "ь"-> ""
+                        "э"-> "e"
+                        "ю"-> "yu"
+                        "я"-> "ya"
+
+
+
+
+                        else -> it.value
+
+                    }
+                }
+
+
+        return Tt
 
     }
 
