9d715ed8af248bb55f347b1cb457df5321111229
dino944 <dino944@gmail.com>
2019-07-08 12:31:02 +0300

    hometask_2 complete10

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -21,9 +21,16 @@ abstract class BaseMessage (
         fun makeMessage(from: User?, chat:Chat, date:Date = Date(), type:String="text", payload: Any?, isIncoming: Boolean = false): BaseMessage{
             lastId++
             return when(type){
+
+                "image"-> ImageMessage("${lastId++}", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
+                "text" -> TextMessage("${lastId++}", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
+
+                else-> if ("image" == payload || "text" == payload)
+                    makeMessage(from, chat, date, payload, type, isIncoming) else throw IllegalArgumentException()
+                /*
                 "image"-> ImageMessage("$lastId", from, chat, date=date, image=payload as String, isIncoming = isIncoming)
                 else -> TextMessage("$lastId", from, chat, date=date, text=payload as String, isIncoming = isIncoming)
-
+*/
             }
         }
     }
