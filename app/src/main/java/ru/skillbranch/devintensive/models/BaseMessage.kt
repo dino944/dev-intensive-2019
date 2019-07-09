fcbc414477efb31ace2e46848dcbbf979d438dc6
dino944 <dino944@gmail.com>
2019-07-09 19:42:03 +0300

    hometask_2 complete19

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -13,24 +13,35 @@ abstract class BaseMessage(
     abstract fun formatMessage(): String
 
     companion object abstractFactory {
-
         var lastId = -1
         fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String = "text", payload: Any?,
                         isIncoming: Boolean = false): BaseMessage {
             lastId++
-
+                return when (from) {
+                    null -> throw IllegalArgumentException()
+                    else ->
                 return when (type) {
+                    "image" -> ImageMessage("${lastId}", from, chat, date = date,
+                            image = payload as String, isIncoming = isIncoming)
+                    "text" -> TextMessage("${lastId}", from, chat, date = date,
+                            text = payload as String, isIncoming = isIncoming)
+
+                    else -> if ("image" == payload || "text" == payload)
+                        makeMessage(from, chat, date, payload, type, isIncoming) else throw IllegalArgumentException()
+                }
+/*
                     "image"-> ImageMessage("${lastId}", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
                     "text" -> TextMessage("${lastId}", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
 
                     else-> if ("image" == payload || "text" == payload) makeMessage(from, chat, date, payload, type, isIncoming) else throw IllegalArgumentException()
-                    /*
+*/
+/*
                     "image" -> ImageMessage("$lastId", from, chat, date = date,
                             image = payload as String, isIncoming = isIncoming)
                     else -> TextMessage("$lastId", from, chat, date = date,
                             text = payload as String, isIncoming = isIncoming)
-
 */
+
 
             }
         }
