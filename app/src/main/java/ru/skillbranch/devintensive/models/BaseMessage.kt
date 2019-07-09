80a25d18209a620c0ed59c1efb906dd921656909
dino944 <dino944@gmail.com>
2019-07-09 17:32:33 +0300

    hometask_2 complete18

--- app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/BaseMessage.kt
@@ -4,22 +4,34 @@ package ru.skillbranch.devintensive.models
 import java.util.*
 
 abstract class BaseMessage(
-        val id: String,
-        val from: User?,
-        val chat: Chat,
-        val isIncoming: Boolean = false,
-        val date: Date = Date()
+        var id: String,
+        var from: User?,
+        var chat: Chat,
+        var isIncoming: Boolean = false,
+        var date: Date = Date()
 ) {
     abstract fun formatMessage(): String
 
     companion object abstractFactory {
+
         var lastId = -1
         fun makeMessage(from: User?, chat: Chat, date: Date = Date(), type: String = "text", payload: Any?,
                         isIncoming: Boolean = false): BaseMessage {
             lastId++
-            return when (type) {
-                "image" -> ImageMessage("$lastId", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
-                else -> TextMessage("$lastId", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
+
+                return when (type) {
+                    "image"-> ImageMessage("${lastId}", from, chat, date = date, image = payload as String, isIncoming = isIncoming)
+                    "text" -> TextMessage("${lastId}", from, chat, date = date, text = payload as String, isIncoming = isIncoming)
+
+                    else-> if ("image" == payload || "text" == payload) makeMessage(from, chat, date, payload, type, isIncoming) else throw IllegalArgumentException()
+                    /*
+                    "image" -> ImageMessage("$lastId", from, chat, date = date,
+                            image = payload as String, isIncoming = isIncoming)
+                    else -> TextMessage("$lastId", from, chat, date = date,
+                            text = payload as String, isIncoming = isIncoming)
+
+*/
+
             }
         }
     }
--- app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/TextMessage.kt
@@ -11,7 +11,9 @@ class TextMessage(
         isIncoming: Boolean = false,
         date: Date = Date(),
         var text: String?
-) : BaseMessage (id, from, chat, isIncoming, date){
-    override fun formatMessage(): String ="id:$id ${from?.firstName} ${if (isIncoming) "получил" else "отправил"} сообщение \"$text \" ${date.humanizeDiff()}"
-}
+) : BaseMessage (id, from, chat, isIncoming, date) {
 
+        override fun formatMessage(): String = "id:$id ${from?.firstName} ${if (isIncoming) "получил" else "отправил"}" +
+                " сообщение \"$text \" ${date.humanizeDiff()}"
+
+}
